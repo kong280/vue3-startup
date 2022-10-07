ec5cdac4e8399459af3bc6cab06d5b89b8addfba
unknow <unknow@gmail.com>
2022-10-08 00:09:37 +0800

    vue3+vite+ts项目初始化,集成vue-router,element-plus

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+
+
+    <el-space wrap>
+      <router-link to="/">Home</router-link>
+      <el-divider direction="vertical" />
+      <router-link to="/about">About</router-link>
+    </el-space>
+
+    <router-view></router-view>
+
+    <div class="footer">
+      <HelloWorld msg="this is footer" />
+    </div>
+  </div>
+
+
+</template>
+
+<style scoped>
+.wrapper {
+  width: 1000px;
+  margin: 20px auto;
+}
+
+.footer {
+  background-color: blanchedalmond;
+  padding: 20px;
+  margin-top: 20px;
+}
+</style>
